94d9cd58f944b5ceef800be5bfd39635d9c54143
Denis <therealpanda98@gmail.com>
2021-10-04 00:35:58 +0300

    wrote test for auth guards

--- apps/shell/src/app/guards/authorized.guard.spec.ts
+++ apps/shell/src/app/guards/authorized.guard.spec.ts
@@ -1,16 +1,54 @@
 import { TestBed } from '@angular/core/testing';
-
 import { AuthorizedGuard } from './authorized.guard';
+import { Router } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { UserService } from '@nx-mfe/shared/data-access-user';
+import { of } from 'rxjs';
 
-describe('AuthGuard', () => {
-  let guard: AuthorizedGuard;
+describe(AuthorizedGuard, () => {
+	let guard: AuthorizedGuard;
+	let userService: UserService;
+	let router: Router;
+	const routerMock = { navigate: jest.fn() };
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    guard = TestBed.inject(AuthorizedGuard);
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			providers: [
+				AuthorizedGuard,
+				{ provide: Router, useValue: routerMock },
+			],
+			imports: [HttpClientTestingModule],
+		}).compileComponents();
 
-  it('should be created', () => {
-    expect(guard).toBeTruthy();
-  });
+		guard = TestBed.inject(AuthorizedGuard);
+		userService = TestBed.inject(UserService);
+		router = TestBed.inject(Router);
+	});
+
+	it('should be created', () => {
+		expect(guard).toBeTruthy();
+	});
+
+	it('should redirect an authenticated user to the login page', (done) => {
+		jest.spyOn(userService, 'isUserLoggedIn$', 'get').mockReturnValue(
+			of(false)
+		);
+
+		guard.canActivate().subscribe((x) => {
+			expect(x).toBeFalsy();
+			expect(router.navigate).toHaveBeenCalledWith(['/login']);
+			done();
+		});
+	});
+
+	it('should allow the authenticated user to access app', (done) => {
+		jest.spyOn(userService, 'isUserLoggedIn$', 'get').mockReturnValue(
+			of(true)
+		);
+
+		guard.canActivate().subscribe((x) => {
+			expect(x).toBeTruthy();
+			done();
+		});
+	});
 });
--- apps/shell/src/app/guards/unauthorized.guard.spec.ts
+++ apps/shell/src/app/guards/unauthorized.guard.spec.ts
@@ -1,16 +1,54 @@
 import { TestBed } from '@angular/core/testing';
-
 import { UnauthorizedGuard } from './unauthorized.guard';
+import { UserService } from '@nx-mfe/shared/data-access-user';
+import { Router } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
 
-describe('UnauthorizedGuard', () => {
-  let guard: UnauthorizedGuard;
+describe(UnauthorizedGuard, () => {
+	let guard: UnauthorizedGuard;
+	let userService: UserService;
+	let router: Router;
+	const routerMock = { navigate: jest.fn() };
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    guard = TestBed.inject(UnauthorizedGuard);
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			providers: [
+				UnauthorizedGuard,
+				{ provide: Router, useValue: routerMock },
+			],
+			imports: [HttpClientTestingModule],
+		}).compileComponents();
 
-  it('should be created', () => {
-    expect(guard).toBeTruthy();
-  });
+		guard = TestBed.inject(UnauthorizedGuard);
+		userService = TestBed.inject(UserService);
+		router = TestBed.inject(Router);
+	});
+
+	it('should be created', () => {
+		expect(guard).toBeTruthy();
+	});
+
+	it('should redirect an authenticated user to the home page', (done) => {
+		jest.spyOn(userService, 'isUserLoggedIn$', 'get').mockReturnValue(
+			of(true)
+		);
+
+		guard.canActivate().subscribe((x) => {
+			expect(x).toBeFalsy();
+			expect(router.navigate).toHaveBeenCalledWith(['/']);
+			done();
+		});
+	});
+
+	it('should allow the unauthenticated user to access login page', (done) => {
+		jest.spyOn(userService, 'isUserLoggedIn$', 'get').mockReturnValue(
+			of(false)
+		);
+
+		guard.canActivate().subscribe((x) => {
+			expect(x).toBeTruthy();
+			done();
+		});
+	});
 });
