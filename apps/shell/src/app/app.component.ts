75bfa19125ca33f6e71f383f8425776cba9b0a09
Denis <therealpanda98@gmail.com>
2021-10-04 00:33:54 +0300

    fixing pre commit

--- apps/shell/src/app/app.component.ts
+++ apps/shell/src/app/app.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'nx-mfe-root',
-	template: `
-		<router-outlet></router-outlet>
-	`,
+	template: ` <router-outlet></router-outlet> `,
 	styles: [],
 })
-export class AppComponent {
-	public readonly title = 'Admin Dashboard';
-}
+export class AppComponent {}
