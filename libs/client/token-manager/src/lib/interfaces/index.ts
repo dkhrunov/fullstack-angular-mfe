b0a225d887c07ace85180f77943b5601d2ebfde8
Denis Khrunov <dkhrunov@tenderhub.ru>
2022-04-27 10:30:15 +0300

    fix token manager lib

--- libs/client/token-manager/src/lib/interfaces/index.ts
+++ libs/client/token-manager/src/lib/interfaces/index.ts
@@ -1,4 +1,2 @@
 export * from './auth-tokens.interface';
-export * from './cookie-builder.interface';
-export * from './cookie-options.interface';
 export * from './token-manager-options.interface';
